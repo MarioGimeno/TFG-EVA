2116fa0966c48f55b7e9118216f492925d2f52f2
MarioGimeno <mgimenonavaz@hotmail.com>
2025-01-09 09:03:36 +0100

    Canbios para cloud

--- Back-end/server.js
+++ Back-end/server.js
@@ -23,6 +23,7 @@ const fields = [
     { name: 'location', maxCount: 1 }
 ];
 ffmpeg.setFfmpegPath(path.join(__dirname, 'ffmpeg/bin/ffmpeg.exe'));
+//En EC2 ubuntu tendra que ser ffmpeg.setFfmpegPath('/usr/bin/ffmpeg');
 
 const PORT = process.env.PORT || 3000;
 
