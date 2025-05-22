664f61bc392ca59a2ecbf28722fdd7cf35eca0d2
MarioGimeno <mgimenonavaz@hotmail.com>
2025-05-22 22:57:16 +0200

    a

--- Back-end/routes/contacts.js
+++ Back-end/routes/contacts.js
@@ -1,20 +1,15 @@
-// src/routes/contacts.js
-const express   = require('express');
-const router    = express.Router();
-const auth      = require('../middleware/authMiddleware');
-const ctrl      = require('../controllers/contactscontroller');
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/authMiddleware');
+const contactsController = require('../controllers/contactscontroller');
 
-// Debug middleware (opcional)
 router.use((req, res, next) => {
   console.log('🛎️ Contacts router:', req.method, req.originalUrl);
   next();
 });
 
-router.get(    '/',    auth, ctrl.list);
-router.post('/', auth, (req, res, next) => {
-  console.log('Middleware justo antes del controlador en POST /api/contacts');
-  next();
-}, ctrl.create);
-router.delete('/:id',  auth, ctrl.delete);
+router.get('/', auth, contactsController.list);
+router.post('/', auth, contactsController.create);
+router.delete('/:id', auth, contactsController.delete);
 
 module.exports = router;
