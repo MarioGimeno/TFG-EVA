933ff878b6965a7885244b165354377df0876577
MarioGimeno <mgimenonavaz@hotmail.com>
2025-05-22 22:40:25 +0200

    Update ContactsService.js

--- Back-end/services/ContactsService.js
+++ Back-end/services/ContactsService.js
@@ -8,22 +8,31 @@ class ContactsService {
     async getContacts(userId) {
       return this.contactsRepository.findAllByUserId(userId);
     }
-  
-    async addContact(contactData) {
-      console.log('Buscando email:', contactData.email);
-      try {
-        const userId = await this.contactsRepository.findUserIdByEmail(contactData.email);
-        // lógica para añadir contacto usando userId
-      } catch (error) {
-        if (error.message === 'El email no existe') {
-          const err = new Error('El email no existe');
-          err.statusCode = 404;
-          throw err;
-        }
-        throw error;
-      }
+ async addContact(ownerId, contactData) {
+  try {
+    // Busca el userId del email del contacto
+    const contactUserId = await this.contactsRepository.findUserIdByEmail(contactData.email);
+
+    // Crea el contacto con los datos
+    const newContact = await this.contactsRepository.createContact(
+      ownerId,
+      contactData.name,
+      contactData.email,
+      contactUserId
+    );
+
+    return newContact;
+
+  } catch (error) {
+    if (error.message === 'El email no existe') {
+      const err = new Error('El email no existe');
+      err.statusCode = 404;
+      throw err;
     }
-  
+    throw error;
+  }
+}
+
     async removeContact(userId, contactId) {
       await this.contactsRepository.deleteContact(contactId, userId);
     }
