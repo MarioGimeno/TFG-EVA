153aa6791e07daf077139c6c4c3a4073da03d17c
MartaPerezCortes <161013514+MartaPerezCortes@users.noreply.github.com>
2025-05-24 14:10:08 +0200

    alerta

--- Front-end/app/src/main/java/com/example/appGrabacion/App.java
+++ Front-end/app/src/main/java/com/example/appGrabacion/App.java
@@ -4,6 +4,10 @@ import android.app.Application;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
+import android.content.ContentResolver;
+import android.media.AudioAttributes;
+import android.net.Uri;
+
 
 public class App extends Application {
     @Override
@@ -15,6 +19,15 @@ public class App extends Application {
                     "Live Location",
                     NotificationManager.IMPORTANCE_HIGH
             );
+            Uri soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://"
+                    + getPackageName() + "/raw/alerta");
+            AudioAttributes audioAttrs = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+            channel.setSound(soundUri, audioAttrs);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{0, 500, 200, 500});
             NotificationManager nm = getSystemService(NotificationManager.class);
             nm.createNotificationChannel(channel);
         }
